13bb948a3cc2feb6041db80bc6692fba272af923
abkmac <abhishekmacwan342@gmail.com>
2023-07-07 19:39:40 +0530

    solved the register error

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
+app.use(express.json());
 app.post('/register',(req,res)=>{
-    res.json('test2 ok');
+    const{username,password}=req.body;
+    res.json({username,password});
+    console.log(res.json({username,password}));
 })
 
 app.listen(4000);
